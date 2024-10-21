c084eb770b04a548d49e882a845549b5a0c27f74
mengen.dai <mengen.dai@outlook.com>
2024-10-21 23:04:53 +0800

    同时捕获扬声器和麦克风音频- 修改 catcher_microphone 函数，将音频设备索引从 0 改为 1
    - 在 main 函数中添加多线程处理，同时捕获扬声器和麦克风音频
    - 注释掉的代码用于打印输入和输出音频设备列表

--- src/example.rs
+++ src/example.rs
@@ -1,10 +1,10 @@
-use crate::utils::audio_device::AudioDeviceType;
 use crate::utils::audio_device;
+use crate::utils::audio_device::AudioDeviceType;
 use std::env;
 
 pub fn catcher_microphone() {
     let path = env::current_dir().unwrap().join("recorded_microphone.wav");
-    audio_device::catcher_device_audio(AudioDeviceType::Input, 0, path);
+    audio_device::catcher_device_audio(AudioDeviceType::Input, 1, path);
 }
 
 pub fn catcher_speaker() {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use audio_catcher::example;
 
 fn main() {
-    example::catcher_speaker();
+    // example::catcher_speaker();
     // example::catcher_microphone();
+    let catcher_speaker_thread = std::thread::spawn(example::catcher_speaker);
+    let catcher_microphone_thread = std::thread::spawn(example::catcher_microphone);
+    catcher_speaker_thread.join().unwrap();
+    catcher_microphone_thread.join().unwrap();
+
+    // for audio_device in audio_device::get_input_devices() {
+    //     println!("{}", audio_device.device.name().unwrap())
+    // }
+    //
+    // for audio_device in audio_device::get_output_devices() {
+    //     println!("{}", audio_device.device.name().unwrap())
+    // }
 }
 
